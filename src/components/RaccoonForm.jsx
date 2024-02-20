22ec9b090cbd6662cf1425da57385ef77edb4ffa
brewchetta <chett.tiller@gmail.com>
2024-02-20 15:50:57 -0500

    major styling, added some sounds

---
+++ src/components/RaccoonForm.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+
+function RaccoonForm({ setRaccoonsArr, raccoonsArr }) {
+
+    const [name, setName] = useState("")
+    const [location, setLocation] = useState("")
+    const [img_url, setImgUrl] = useState("")
+
+    function handleChangeName(event) {
+        setName(event.target.value)
+    }
+    
+    function handleChangeLocation(event) {
+        setLocation(event.target.value)
+    }
+
+    function handleChangeImageUrl(event) {
+        setImgUrl(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const bodyObj = {name, location, img_url}
+
+        fetch("http://localhost:5555/raccoons", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(bodyObj)
+        })
+        .then( res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                throw new Error("Something went wrong!")
+            }
+        })
+        .then( data => setRaccoonsArr([...raccoonsArr, data]) )
+        .catch(err => alert(err))
+    }
+
+    return (
+        <form className="new-raccoon-form" onSubmit={handleSubmit}>
+
+            <label htmlFor="name">Name:</label>
+            <input type="text" 
+            name="name" 
+            onChange={handleChangeName} 
+            value={name}
+            placeholder="raccoon name" />
+
+            <label htmlFor="location">Location:</label>
+            <input type="text" 
+            name="location" 
+            onChange={handleChangeLocation} 
+            value={location}
+            placeholder="raccoon location" />
+
+            <label htmlFor="img_url">Image:</label>
+            <input type="text" 
+            name="img_url" 
+            onChange={handleChangeImageUrl} 
+            value={img_url}
+            placeholder="image url" />
+
+            <br/>
+
+            <input type="submit" value="Add Raccoon" />
+
+        </form>
+    )
+
+}
+
+export default RaccoonForm
---
+++ src/components/RaccoonsContainer.jsx
@@ -0,0 +1,17 @@
+import RaccoonCard from './RaccoonCard'
+
+function RaccoonsContainer({ raccoonsArr, audioOn }) {
+
+    const mappedRaccoons = raccoonsArr.map(raccoon => <RaccoonCard key={raccoon.id} raccoon={raccoon} audioOn={audioOn} />)
+
+    return (
+        <div>
+
+        { mappedRaccoons }
+
+        </div>
+    )
+
+}
+
+export default RaccoonsContainer
